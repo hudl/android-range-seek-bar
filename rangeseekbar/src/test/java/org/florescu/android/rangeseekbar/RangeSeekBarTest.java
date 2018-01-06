896791ba4f95c656af0492656079c4d5b4154d62
Debbie Beliveau <debbie.beliveau@hudl.com>
2018-01-05 16:42:35 -0800

    Update build tools, support libs, test libs, & sdk
    * Update Android Plugin for Gradle to 3.0.1
    * Update Gradle to 4.1
    * Update compile and target sdk to 26
    * Update build tools to 26.0.2
    * Update support libs to 26.0.2
    * Update roboelectric to 3.6.1
    * Explicitly include junit4 now that it's not included by new version
      of roboelectric

--- rangeseekbar/src/test/java/org/florescu/android/rangeseekbar/RangeSeekBarTest.java
+++ rangeseekbar/src/test/java/org/florescu/android/rangeseekbar/RangeSeekBarTest.java
@@ -3,12 +3,12 @@ package org.florescu.android.rangeseekbar;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
-@RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 21)
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 26)
 public class RangeSeekBarTest {
 
     @Test
